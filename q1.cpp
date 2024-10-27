9cc7f2d8135477c093276def3ba012231b6962e7
DiceAntagonist <126333279+DiceAntagonist@users.noreply.github.com>
2024-10-27 18:55:01 +0530

    Create q1.cpp

---
+++ q1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+bool FA(string w) { 
+    int state = 0; 
+
+ 
+    for (int i = 0; i < w.length(); i++) {
+        char c = w[i];
+
+        switch (state) {
+            case 0:
+                if (c == '1') state = 1;  
+                else state = 0;
+                break;
+            case 1:
+                if (c == '1') state = 2;  
+                else state = 0; 
+                break;
+            case 2:
+                if (c == '1') state = 3; 
+                else state = 0; 
+                break;
+            case 3:
+                if (c == '1') state = 4;   
+                else state = 0;  
+                break;
+            case 4:
+           
+                return false; 
+        }
+    }
+
+    return (state == 3);
+}
+
+int main() {
+    string w;
+    cout << "Enter a binary string: ";
+    cin >> w;
+
+    if (FA(w)) {
+        cout << "String accepted " << endl;
+    } else {
+        cout << "String rejected " << endl;
+    }
+
+    return 0;
+}
