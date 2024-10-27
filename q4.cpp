a69c95e02a38293ad67b488d1b55e5c26c438a4f
DiceAntagonist <126333279+DiceAntagonist@users.noreply.github.com>
2024-10-27 21:30:32 +0530

    Create q4.cpp

---
+++ q4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool FA(string w) {
+    int n = w.length();
+    return (n >= 2 && w[0] == 'a' && w[n - 1] == 'b');
+}
+
+int main() {
+    string w;
+    cout << "Enter a string over {a, b}: ";
+    cin >> w;
+
+    if (FA(w)) {
+        cout << "String accepted" << endl;
+    } else {
+        cout << "String rejected" << endl;
+    }
+
+    return 0;
+}
