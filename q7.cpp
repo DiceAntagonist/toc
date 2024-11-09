a72fcdcc4f6285017ae2ee88b56253098c2212e0
DiceAntagonist <126333279+DiceAntagonist@users.noreply.github.com>
2024-11-09 23:33:58 +0530

    Create q7.cpp

---
+++ q7.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+bool simulatePDA(string input) {
+ stack<char> pdaStack;
+ for (char c : input) {
+ if (c == 'a') {
+ pdaStack.push('a');
+ }
+ else if (c == 'b') {
+ if (pdaStack.empty()) {
+ return false;
+ }
+ pdaStack.pop();
+ }
+ else {
+ return false;
+ }
+ }
+ return pdaStack.empty();
+}
+int main() {
+ string input;
+ cout << "Enter a string over {a, b} in the format a^n b^n: ";
+ cin >> input;
+ if (simulatePDA(input)) {
+ cout << "String accepted by the PDA." << endl;
+ }
+ else {
+ cout << "String rejected by the PDA." << endl;
+ }
+ return 0;
+}
