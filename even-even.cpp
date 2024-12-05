71737e51e704dbd60a267b256d785db54a2c633c
DiceAntagonist <126333279+DiceAntagonist@users.noreply.github.com>
2024-12-06 01:03:57 +0530

    Update even-even.cpp

--- even-even.cpp
+++ even-even.cpp
@@ -1,26 +1,43 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 using namespace std;
 
-bool FA(string w) {
-    int count_a = 0, count_b = 0;
-    for (char c : w) {
-        if (c == 'a') count_a++;
-        else if (c == 'b') count_b++;
+bool ab (string c){
+    for (int i = 0; i<c.length(); i++ ){
+        if(c[i]!='a' && c[i]!='b'){
+            return false;
+        }
     }
-    return (count_a % 2 == 0 && count_b % 2 == 0);
+    return true;
+}
+
+bool FA (string c){
+    if(ab(c)){
+        int counta = 0;
+        int countb = 0;
+        
+        for (int i = 0; i<c.length(); i++){
+            if(c[i]=='a'){
+                counta++;
+            }
+            else if(c[i]=='b'){
+                countb++;
+            }
+        }
+        if (counta%2==0 && countb%2==0){
+            return true;
+        }
+    }
+    return false;
 }
 
 int main() {
-    string w;
-    cout << "Enter a string over {a, b}: ";
-    cin >> w;
-
-    if (FA(w)) {
-        cout << "String accepted" << endl;
-    } else {
-        cout << "String rejected" << endl;
-    }
-
+    
+ cout<<"enter string";
+    string c;
+    cin>>c;
+   if(FA(c)){ cout<<"string accepted";}
+   else{cout<<"string rejected";}
+   
+    
     return 0;
 }
